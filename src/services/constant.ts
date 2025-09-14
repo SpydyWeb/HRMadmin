2cdae08d54748d45420c011433a4d4d04bcc04f0
SpydyWeb <70287710+SpydyWeb@users.noreply.github.com>
2025-09-14 11:55:34 +0530

    doen

--- src/services/constant.ts
+++ src/services/constant.ts
@@ -1,7 +1,7 @@
 export const TOKEN_KEY = 'auth_token'
 export const APIRoutes = {
-  // BASEURL:"/api/",
-  BASEURL:"http://ezytek1706-003-site1.rtempurl.com/api/",
+  BASEURL:"/api/",
+  // BASEURL:"http://ezytek1706-003-site1.rtempurl.com/api/",
   LOGIN: 'Auth/Login',
   AGENTSEARCH: 'Agent/Search',
   AGENTBYCODE: "/Agent/AgentByCode", 
