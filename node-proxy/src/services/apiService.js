05055055176f9838db1a69f694d951c58702da75
SpydyWeb <70287710+SpydyWeb@users.noreply.github.com>
2025-10-01 13:59:48 +0530

    done

--- node-proxy/src/services/apiService.js
+++ node-proxy/src/services/apiService.js
@@ -1,14 +1,14 @@
 const { apiClient } = require("./apiclient");
-const {APIRoutes} =require("./constant")
+const { APIRoutes } = require("./constant");
 
 const login = (data) => {
   return apiClient.post(APIRoutes.LOGIN, data);
 };
-const search = (data,headers={}) => {
-    return apiClient.post(APIRoutes.AGENTSEARCH, data,{headers});
+const search = (data, headers = {}) => {
+  return apiClient.post(APIRoutes.AGENTSEARCH, data, { headers });
 };
-const searchbycode = (data,headers={}) => {
-    return apiClient.post(APIRoutes.AGENTBYCODE, data,{headers});
+const searchbycode = (data, headers = {}) => {
+  return apiClient.post(APIRoutes.AGENTBYCODE, data, { headers });
 };
 
-module.exports={login,search,searchbycode}
+module.exports = { login, search, searchbycode };
--- node-proxy/src/services/constant.js
+++ node-proxy/src/services/constant.js
@@ -1,6 +1,6 @@
 export const APIRoutes = {
-  // BASEURL:"/api/",
-  BASEURL:"http://ezytek1706-003-site1.rtempurl.com/api/",
+  BASEURL:"/api/",
+  // BASEURL:"http://ezytek1706-003-site1.rtempurl.com/api/",
   LOGIN: 'Auth/Login',
   AGENTSEARCH: 'Agent/Search',
   AGENTBYCODE: "/Agent/AgentByCode", 
