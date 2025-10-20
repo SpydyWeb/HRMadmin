40ff35827354e693d072439bd61b19755b4bb1a0
SpydyWeb <70287710+SpydyWeb@users.noreply.github.com>
2025-10-20 11:55:42 +0530

    done

--- Admin-ui/src/auth.ts
+++ Admin-ui/src/auth.ts
@@ -41,10 +41,10 @@ export const auth = {
   },
   async login(data: ILoginRequest): Promise<ApiResponse<ILoginResponseBody>> {
     const response = await authService.login(data)
-    const token = response.responseBody.loginResponse?.token
+    const token = response.responseBody.loginResponse
     if (token) {
       _token = token
-      storage.set(TOKEN_KEY, JSON.stringify(response.responseBody.loginResponse))
+      storage.set(TOKEN_KEY, JSON.stringify(token))
     }
     return response
   },
