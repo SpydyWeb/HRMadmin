4f06b29e6343046949aed86c165a74849ac2e8d8
Gaurav Rathore <gauravrathore1508@gmail.com>
2026-04-16 14:25:11 +0530

    updatedENV

--- node-proxy/src/config/index.js
+++ node-proxy/src/config/index.js
@@ -12,11 +12,11 @@ module.exports = {
   cors: {
     allowedOrigins: [
       "https://hrmadmin.netlify.app",
-      "https://hms-alpha-eight.vercel.app",
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      process.env.VITE_API_URL
     ],
   },  // Example: 16-char IV
   // console.log("test")
